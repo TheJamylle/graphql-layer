94d7da1c8b1e6f3722b60903299ba099bb735938
TheJamylle <jamyllemagal@gmail.com>
2020-10-09 16:44:26 -0300

    Commit inicial

---
+++ src/index.js
@@ -0,0 +1,61 @@
+const express = require('express')
+const { graphqlExpress, graphiqlExpress } = require('graphql-server-express')
+const { makeExecutableSchema } = require('graphql-tools')
+// const mongoose = require('mongoose')
+const Redis = require('ioredis')
+const bodyParser = require('body-parser')
+
+// mongoose.connect('mongodb://localhost/test', {userNewUrlParser: true})
+
+// const db = mongoose.connection
+
+const redis = new Redis()
+
+// db.on('error', console.error.bind(console, 'connection error:'))
+// db.once('open', function() {});
+
+const schema = require('./schema')
+
+const resolvers = {
+    Query: {
+        get: (parent, {key}, {redis}) => {
+          try {
+            return redis.get(key)
+          } catch (error) {
+            return null
+          }
+        }
+      },
+    
+      Mutation: {
+        set: async (parent, {key, value}, {redis}) => {
+          try {
+            await redis.set(key, value)
+            return true
+          } catch (error) {
+            console.log(error)
+            return false
+          }
+        }
+      }
+}
+
+const app = express()
+
+const executableSchema = makeExecutableSchema({
+  typeDefs: [schema],
+  resolvers,
+  context: request => {
+    return {
+      ...request
+    }
+  }
+})
+
+app.use('/graphql', bodyParser.json(), graphqlExpress({
+  schema: executableSchema, context: { redis }
+}))
+
+app.use('/graphiql', graphiqlExpress({ endpointURL: '/graphql' }))
+
+app.listen(8888, () => console.log('Dessa vez tá rodando na 8888'))
---
+++ src/schema.js
@@ -0,0 +1,8 @@
+module.exports = `type Query {  
+  get(key: String!): String
+}
+
+type Mutation {
+  set(key: String!, value:  String!): Boolean!
+}
+`;
